c43e4b06a8ef16fe12bd8fa1c06d9f2783bd6811
akashfd <ahsanshamim193@gmail.com>
2024-04-24 15:37:00 +0600

    opeartor arithmethic practice

---
+++ operator.js
@@ -0,0 +1,70 @@
+// pre incriment operator
+let houseprice =12031
+let carprice= 2013
+let total=houseprice+ ++carprice
+console.log (total)
+
+// pre decriment operator
+let follower=2012
+let following=2014
+let member=follower+  --following
+console.log(member)
+
+// post incriment operator
+let boys=45
+let girl=65
+let person= boys+ girl--
+console.log(person)
+
+// post decriment operator
+let books=100
+let pens=50
+let library= books- pens--
+console.log(library)
+
+// applying arithmrthic operation(numeric string)
+let classOne="10"
+let classTwo="2"
+let student=classOne+classTwo
+console.log(student)
+
+
+let cow="10"
+let tiger="2"
+let farmAnimal= cow-tiger
+console.log(farmAnimal)
+
+
+let pizza="10"
+let guest="2"
+let table= pizza/guest
+console.log(table)
+
+let birds="10"
+let tree="2"
+let cluster= birds*tree
+console.log(cluster)
+
+let car="10"
+let bike="2"
+let vehicle= car%bike
+console.log (vehicle)
+
+
+// applying arithmrthic operation(text string)
+let textOne="hello"
+let textTwo="world"
+let sentence= textOne+textTwo
+console.log(sentence)
+
+let wordOne ="hello"
+let wordTwo="world"
+let line= wordOne+ " " + wordTwo
+console.log(line)
+
+let institution= "pondit"
+let mentor="asif"
+let courseMinus=institution-mentor
+let courseMultiplication=institution*mentor
+let courseDivision=institution/mentor
+console.log(courseMinus,courseMultiplication,courseDivision )
